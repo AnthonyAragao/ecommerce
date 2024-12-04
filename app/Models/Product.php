5dc12db203d469154ff971b94b40644add4c0d52
Anthony Aragão <anthony.amoz.s.a@gmail.com>
2024-12-04 09:48:57 -0300

    feat: adicionado models de Address, Category, Order, OrderItem, Payment, Product, ProductImage e Review, e relacionamentos na model Product

---
+++ app/Models/Product.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $table = 'products';
+
+    protected $fillable = ['name', 'slug', 'description', 'price', 'stock', 'category_id'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+
+    public function images()
+    {
+        return $this->hasMany(ProductImage::class, 'product_id');
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class, 'product_id');
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class, 'product_id');
+    }
+}
